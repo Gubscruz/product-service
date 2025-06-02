61c5fd0d0d68b78ec45117d1ea3f4c6ea50bfac2
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2025-06-02 16:15:11 -0300

    Refactor Product class to implement Serializable and improve code structure

--- src/main/java/store/product/Product.java
+++ src/main/java/store/product/Product.java
@@ -1,14 +1,16 @@
 package store.product;
 
+import java.io.Serializable;
 import java.util.Date;
-
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Builder
-@Data @Accessors(fluent = true)
-public class Product {
+@Data
+@Accessors(fluent = true)
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private String idProduct;
     private String name;
@@ -17,5 +19,4 @@ public class Product {
     private Double price;
     private Date creation;
     private Date update;
-    
 }
